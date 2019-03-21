19fbaff515fdecdd8472ee896f1b56c821b698e6
Adrien <napalm_ground@hotmail.com>
2019-03-21 17:15:10 +0100

    backup

--- demineur.js
+++ demineur.js
@@ -4,6 +4,28 @@ var timer;
 var difficulty = "beginner";
 var tableau = [];
 
+var totalBombs;
+
+
+// https://www.w3schools.com/js/js_htmldom_methods.asp
+// https://www.w3schools.com/jsref/default.asp
+// https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+
+
+function randomNumber(maxNumber)
+{
+	return Math.floor(maxNumber * Math.random()) /*+ 1*/;
+}
+
+class Tile {
+	constructor() {
+		this.hasBomb = false;
+		this.uncovered = false;	// Set to false
+		this.adjBombs = 0;
+	}
+}
+
 
 function chrono() {
 	var currentTime = Date.now();
@@ -30,15 +52,23 @@ function diffcultySetup(){
 	switch (difficulty){
 		case "beginner":
 			setupBoard(9);
+			totalBombs = 10;
+			setupBombs(9, totalBombs);
 			break;
 		case "medium":
 			setupBoard(16);
+			totalBombs = 40;
+			setupBombs(16, totalBombs);
 			break;
 		case "expert":
 			setupBoard(22);
+			totalBombs = 100;
+			setupBombs(22, totalBombs);
 			break;
 		case "master":
 			setupBoard(30);
+			totalBombs = 250;
+			setupBombs(30, totalBombs);
 			break;
 	}
 }
@@ -50,10 +80,28 @@ function setupBoard(size){
 		var colonne = [];
 
 		for (var j = 0; j < size; j++){
-			colonne.push("X");
+			colonne.push(new Tile());
 		}
 
 		tableau.push(colonne);
+	}
+}
+
+
+function setupBombs(tableauSize, maxBombs){
+
+	for (var i = 0; i < maxBombs; i++){
+
+		var randY = randomNumber(tableauSize);
+		var randX = randomNumber(tableauSize);
+
+		if (tableau[randY][randX].hasBomb == false){
+			tableau[randY][randX].hasBomb = true;
+		}
+		else{
+			i--;
+		}
+
 	}
 }
 
@@ -83,11 +131,28 @@ function displayBoardOnPage(){
 
 		for (var j = 0; j < tableau[i].length; j++){
 
-			var tile = document.createElement('th');
-			var normalTile = new Image();
-			normalTile.src = './images/normal.png'
+			var tile = document.createElement('td');
 
-			line.appendChild(normalTile);
+			tile.dataset.row = "" + i;
+			tile.dataset.column = "" + j;
+
+			var displayTile = new Image();
+
+			if (tableau[i][j].uncovered == false){
+				displayTile.src = './images/normal.png';
+				line.appendChild(displayTile);
+			}
+			else if (tableau[i][j].hasBomb == true){
+				displayTile.src = './images/bomb.png';
+				line.appendChild(displayTile);
+				// YOU LOOSE !!!
+			}
+			else{
+				displayTile.src = './images/empty.png';
+				line.appendChild(displayTile);
+			}
+
+			// var currentTile = document.getElementById("tableauDemineur");
 		}
 	}
 }
@@ -97,8 +162,8 @@ function start() {
 	startChrono();
 	tableau = [];
 	diffcultySetup();
-	displayBoardOnConsole();
 	displayBoardOnPage();
+	console.log(totalBombs);
 }
 
 /*
